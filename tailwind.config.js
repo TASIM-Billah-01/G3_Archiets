20ee4544658d5366ea6ce2d237b738fae789e427
Tasim <tasimbillah611@gmail.com>
2024-08-31 21:16:47 +0600

    architecture homepage using tailwind. it's not responsive yet

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      // With some build tools, your CSS will rebuild
+      // any time *any* file in `src` changes.
+      './src/**/*.{html,js}',
+    ],
+    // ...
+  }
